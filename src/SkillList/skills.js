99a91dbc637f4403c9d0b182acf06f11cb5ba842
Ognyan Angelov <ognyan_angelov@yahoo.com>
2017-12-25 01:57:23 +0200

    Update Dec 2017 info.

--- src/SkillList/skills.js
+++ src/SkillList/skills.js
@@ -1,6 +1,15 @@
 export default {
   "data": [
     {
+      "source": "Home",
+      "skills": [
+        {"name": "Machine Learning", "level": "3/5"},
+        {"name": "Statistics", "level": "3/5"},
+        {"name": "Scala", "level": "2/5"},
+        {"name": "Spark", "level": "1/5"}
+      ]
+    },
+    {
       "source": "Chobolabs",
       "skills": [
         {"name": "Javascript", "level": "4/5"},
@@ -8,6 +17,16 @@ export default {
         {"name": "AWS", "level": "3/5"},
         {"name": "DSL", "level": "2/5"},
         {"name": "PostgreSQL", "level": "2/5"}
+      ]
+    },
+    {
+      "source": "FMI",
+      "skills": [
+        {"name": "Math", "level": "3/5"},
+        {"name": "Analytical Thinking", "level": "4/5"},
+        {"name": "Python", "level": "4/5"},
+        {"name": "Django", "level": "4/5"},
+        {"name": "Functional Prog.", "level": "4/5"}
       ]
     },
     {
@@ -21,20 +40,12 @@ export default {
       ]
     },
     {
-      "source": "FMI",
-      "skills": [
-        {"name": "Math", "level": "3/5"},
-        {"name": "Python", "level": "4/5"},
-        {"name": "Django", "level": "4/5"},
-        {"name": "Functional Prog.", "level": "4/5"}
-      ]
-    },
-    {
       "source": "Buzlylabs",
       "skills": [
-
-        {"name": "Objective-C", "level": "3/5"},
         {"name": "iOS", "level": "3/5"},
+        {"name": "Objective-C", "level": "3/5"},
+        {"name": "Rapid Prototyping", "level": "4/5"},
+        {"name": "Multiplayer Model", "level": "2/5"},
         {"name": "Box2d", "level": "2/5"}
       ]
     }
